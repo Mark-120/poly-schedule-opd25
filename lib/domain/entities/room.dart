0000cb938967e0d3196b8e5ee543f4958b6fe1be
Alektro3000 <47211459+Alektro3000@users.noreply.github.com>
2025-04-10 18:12:10 +0300

    fix: add building to room

--- lib/domain/entities/room.dart
+++ lib/domain/entities/room.dart
@@ -1,12 +1,13 @@
 import 'package:equatable/equatable.dart';
+import 'building.dart';
 
 class Room extends Equatable {
   final int id;
-  final int buildingId;
   final String name;
+  final Building building;
 
-  const Room({required this.id, required this.buildingId, required this.name});
+  const Room({required this.id, required this.building, required this.name});
 
   @override
-  List<Object?> get props => [id, buildingId, name];
+  List<Object?> get props => [id, building, name];
 }
