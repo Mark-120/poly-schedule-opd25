e89b3748def8d31aa10359a45972b1dfcef18ec2
Mark <klimowmark1401@gmail.com>
2025-04-15 00:39:16 +0300

    add small fixes to the domain repositories

--- lib/domain/repositories/schedule_repository.dart
+++ lib/domain/repositories/schedule_repository.dart
@@ -1,5 +1,4 @@
 import 'package:poly_scheduler/domain/entities/schedule/week.dart';
-
 import '../entities/group.dart';
 import '../entities/teacher.dart';
 import '../entities/room.dart';
@@ -12,7 +11,7 @@ abstract class ScheduleRepository {
   Future<List<Building>> getAllBuildings();
   Future<List<Room>> getAllRoomsOfBuilding(int buildingId);
 
-  Future<Week> getScheduleByTeacher(int teacherId, DateTime time);
-  Future<Week> getScheduleByGroup(int groupId, DateTime time);
-  Future<Week> getScheduleByRoom(RoomId roomId, DateTime time);
+  Future<Week> getScheduleByTeacher(int teacherId, DateTime dayTime);
+  Future<Week> getScheduleByGroup(int groupId, DateTime dayTime);
+  Future<Week> getScheduleByRoom(RoomId roomId, DateTime dayTime);
 }
