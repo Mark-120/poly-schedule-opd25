671a8ec111d2ba784e38b59fdbfc1852de595889
Alektro3000 <47211459+Alektro3000@users.noreply.github.com>
2025-04-14 18:25:09 +0300

    add featured methods

--- lib/domain/repositores/featured_repository.dart
+++ lib/domain/repositores/featured_repository.dart
@@ -5,10 +5,13 @@ import '../entities/room.dart';
 abstract class FeaturedRepository {
   Future<List<Teacher>> getFeaturedTeachers();
   void setFeaturedTeachers(List<int> newTeacherIds);
+  void addFeaturedTeacher(int newTeacherId);
 
   Future<List<Group>> getFeaturedGroups();
   void setFeaturedGroups(List<int> newGroupIds);
+  void addFeaturedGroup(int newGroupId);
 
   Future<List<Room>> getFeaturedRooms();
   void setFeaturedRooms(List<RoomId> newRoomIds);
+  void addFeaturedRoom(RoomId newRoomId);
 }
