ebf67d5652dc088b655f531002ae0b9a3c837e25
Alektro3000 <andgu_ba@mail.ru>
2025-09-21 23:36:26 +0300

    import fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:poly_scheduler/core/logger.dart';
-import 'package:poly_scheduler/core/presentation/bloc_observer.dart';
-import 'package:poly_scheduler/core/presentation/uikit/app_strings.dart';
-import 'package:poly_scheduler/data/models/last_schedule.dart';
-import 'package:poly_scheduler/domain/entities/group.dart';
-import 'package:poly_scheduler/domain/entities/room.dart';
-import 'package:poly_scheduler/domain/entities/teacher.dart';
+import 'core/logger.dart';
+import 'core/presentation/bloc_observer.dart';
+import 'core/presentation/uikit/app_strings.dart';
+import 'data/models/last_schedule.dart';
+import 'domain/entities/group.dart';
+import 'domain/entities/room.dart';
+import 'domain/entities/teacher.dart';
 
 import 'domain/repositories/last_schedule_repository.dart';
 import 'domain/usecases/last_schedule_usecases/save_last_schedule.dart';
