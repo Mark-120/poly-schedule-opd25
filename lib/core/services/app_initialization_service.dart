5ab05e75da15eaf247a3e7ba81fd42f40393de8c
Mark-120 <78369499+Mark-120@users.noreply.github.com>
2025-10-20 18:57:46 +0300

    feat: add Google Crashlytics

--- lib/core/services/app_initialization_service.dart
+++ lib/core/services/app_initialization_service.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import '../../domain/usecases/schedule_usecases/on_app_start.dart';
+import '../../firebase_options.dart';
 import '../../service_locator.dart' as di;
 import '../../service_locator.dart';
 import '../logger.dart';
@@ -13,5 +18,16 @@ class AppInitializationService {
     await di.init();
     await sl<OnAppStart>()();
     Bloc.observer = BlocLogger(sl<AppLogger>());
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    };
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
   }
 }
--- lib/core/services/error_handling_service.dart
+++ lib/core/services/error_handling_service.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 class ErrorHandlingService {
   static void handleError(BuildContext context, String message) {
+    FirebaseCrashlytics.instance.recordError(
+      message,
+      StackTrace.current,
+      fatal: false,
+    );
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
       ScaffoldMessenger.of(
         context,
