321bd8d34ba6304e2f34c014f9f0fff67f5cb284
Mark-120 <78369499+Mark-120@users.noreply.github.com>
2025-11-08 20:07:05 +0300

    feat: Created AppThemes file

---
+++ lib/core/presentation/uikit_2.0/app_themes.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+import 'app_text_styles.dart';
+
+class AppTheme {
+  final Color primaryColor;
+
+  AppTheme(this.primaryColor);
+
+  ThemeData get lightTheme {
+    return ThemeData(
+      canvasColor: Colors.transparent,
+      primaryColor: primaryColor,
+      scaffoldBackgroundColor: AppColors.bgLight,
+      appBarTheme: AppBarTheme(
+        backgroundColor: primaryColor,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      cardTheme: CardTheme(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 0,
+        margin: const EdgeInsets.only(bottom: 12),
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: primaryColor,
+        shape: CircleBorder(),
+        elevation: 0,
+      ),
+      bottomAppBarTheme: BottomAppBarTheme(color: primaryColor),
+      extensions: <ThemeExtension<dynamic>>[
+        AppTextStyles(
+          textPrimaryColor: AppColors.black,
+          textSecondaryColor: AppColors.darkGray,
+          hintColor: AppColors.gray,
+          pickedColor: AppColors.white,
+        ),
+      ],
+    );
+  }
+
+  ThemeData get darkThemee {
+    return ThemeData(
+      canvasColor: Colors.transparent,
+      primaryColor: primaryColor,
+      scaffoldBackgroundColor: AppColors.bgDark,
+      appBarTheme: AppBarTheme(
+        backgroundColor: primaryColor,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      cardTheme: CardTheme(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 0,
+        margin: const EdgeInsets.only(bottom: 12),
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: primaryColor,
+        shape: CircleBorder(),
+        elevation: 0,
+      ),
+      bottomAppBarTheme: BottomAppBarTheme(color: primaryColor),
+      extensions: <ThemeExtension<dynamic>>[
+        AppTextStyles(
+          textPrimaryColor: AppColors.white,
+          textSecondaryColor: AppColors.gray,
+          hintColor: AppColors.gray,
+          pickedColor: AppColors.white,
+        ),
+      ],
+    );
+  }
+}
