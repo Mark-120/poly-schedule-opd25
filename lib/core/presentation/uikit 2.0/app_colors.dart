b3d9779208430784c884aa5e3d49eae198cbb597
Mark-120 <78369499+Mark-120@users.noreply.github.com>
2025-11-08 17:49:19 +0300

    feat: add colors to the new UI-kit

---
+++ lib/core/presentation/uikit 2.0/app_colors.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+class AppColors {
+  // Basic
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color bgLight = Color(0xFFF0F0F0);
+  static const Color lightGray = Color(0xFFDCDCDC);
+  static const Color gray = Color(0xFF848484);
+  static const Color darkGray = Color(0xFF373737);
+  static const Color superDarkGray = Color(0xFF191919);
+  static const Color bgDarkGray = Color(0xFF0C0C0C);
+  static const Color black = Color(0xFF000000);
+  static const Color errorsWarnings = Color(0xFFD94927);
+
+  // Accentc
+  static const Color Vshmop = Color(0xFF990000);
+  static const Color Ie = Color(0xFFED601D);
+  static const Color Gi = Color(0xFFFE9920);
+  static const Color Ifksit = Color(0xFF66B934);
+  static const Color Ryae = Color(0xFF37B34A);
+  static const Color Ibsib = Color(0xFF11542B);
+  static const Color Ispo = Color(0xFF009363);
+  static const Color edit = Color(0xFFFFFFFF);
+  static const Color fim = Color(0xFFDCDCDC);
+  static const Color ikniki = Color(0xFF848484);
+  static const Color ipmeiti = Color(0xFF373737);
+  static const Color fimIE = Color(0xFF191919);
+}
